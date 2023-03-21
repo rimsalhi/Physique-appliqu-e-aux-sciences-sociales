5070fbd447b187ba6e2261e04c4d1c4e54fd2249
rsalhi <rim.salhi@ensae.fr>
2023-03-21 16:42:47 +0000

    commentaires

--- Construction d'un graphe.py
+++ Construction d'un graphe.py
@@ -7,18 +7,20 @@ p=0.05
 
 G=nx.Graph()  #Graphe vide
 
-G.add_nodes_from(range(1,N+1))
+G.add_nodes_from(range(1,N+1)) #Ajout des sommets (agents)
 
 for i in range(1,N+1):
-    G.nodes[i]['currency']='i'
+    G.nodes[i]['currency']='i'.  #Au départ, chaque individu i à une devise 'i'
 
+
+#On ajoute des arêtes avec une probabilité p 
 for i in range(N):
     for j in range(i+1,N):
         if random.random()<=p:
-            G.add_edge(i, j)
+            G.add_edge(i, j). 
 
 
-
+#Affichage du graphe
 options={
     'node_color':'green',
     'edge_color':'grey',
