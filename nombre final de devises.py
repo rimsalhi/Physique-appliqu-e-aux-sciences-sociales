347b2d0cd8dc1294e31cbdeedfb39ba741e3c931
rsalhi <rim.salhi@ensae.fr>
2023-03-21 18:14:56 +0000

    c

--- nombre final de devises.py
+++ nombre final de devises.py
@@ -4,6 +4,30 @@ import networkx as nx
 
 N=100
 
-def currencies_number(G,p):
-    
 
+def social_utility(G):
+    u=0
+    for i in G.nodes():
+        for j in G.neighbors(i):
+            if G.nodes[i]['currency']!=G.nodes[j]['currency']:
+                u+=-0.5
+    return u
+
+
+#Étant donné une probabilité p, on calcule le nombre de devises distinctes dans le système final 
+
+def currencies_number(G):
+    u=social_utility(G)
+    precedent=None
+    while True:
+        precedent=u
+        for i in G.nodes():
+            change_currency(i,G)
+        u=social_utility(G)
+        assert u!=precedent
+    L=[]
+    for i in G.nodes:
+        if G.nodes[i]['currency'] not in L:
+            L.append(G.nodes[i]['currency'])
+    return len(L)
+        
